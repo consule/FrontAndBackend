4311628096a89350fa5c43d6e8003b81bbe637f7
Cristiano <cristianoconsule@gmail.com>
2021-06-24 18:22:39 -0300

    Primeiro Commit

---
+++ api/estudos/Controllers/Infra.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+
+
+namespace estudos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Infra : ControllerBase
+    {
+        [HttpGet]
+        [Route("dataatual")]
+        public  IActionResult GetDataAtual()
+        {
+            var stringConexao = "";
+
+            var sql = "SELECT SYSDATE DATA FROM DUAL";
+
+            OracleConnection _oracleConnection = new OracleConnection(stringConexao);
+
+            _oracleConnection.Open();
+
+            OracleCommand cmd = new OracleCommand(sql, _oracleConnection);
+
+            var dr =  cmd.ExecuteReader();
+            string data = "";
+
+            while (dr.Read())
+            {
+                data = dr["DATA"].ToString();
+            }
+
+            return Ok(data);
+        }
+
+    }
+}
