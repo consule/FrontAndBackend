14ab73cc299f29a458a0406df60e78711b858eb5
Cristiano <cristianoconsule@gmail.com>
2021-06-25 11:41:41 -0300

    Delete finalizado

--- portal/src/app/conteudo/conteudo-delete/conteudo-delete.component.ts
+++ portal/src/app/conteudo/conteudo-delete/conteudo-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ConteudoService } from 'src/app/conteudo.service';
+import { conteudoModel } from '../conteudoModel';
 
 @Component({
   selector: 'app-conteudo-delete',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConteudoDeleteComponent implements OnInit {
 
-  constructor() { }
+  codigo_unico: number
+  modelConteudo: conteudoModel
+
+  constructor(private conteudoService: ConteudoService,  private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.codigo_unico = +this.route.snapshot.paramMap.get('codigo_unico');
+    this.conteudoService.getPorCodigoUnico(this.codigo_unico).subscribe(res => {
+      this.modelConteudo = res[0];
+    })
   }
 
+  delete(titulo, subtitulo, dataPublicacao, codigo_unico) {
+    this.conteudoService.delete(codigo_unico).subscribe(res => {
+      alert(JSON.stringify(res["mensagem"]));
+    });
+  }
+
+  getPorCodigoUnico() {
+    this.conteudoService.getPorCodigoUnico(this.codigo_unico).subscribe(res => {
+      this.modelConteudo = res;
+    })
+  }
 }
